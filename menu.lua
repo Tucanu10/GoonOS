1811fec55eb43f99cd0182ecbc5208d80a3163f5
Tucanu <rockerulmaniac454@gmail.com>
2023-08-17 13:41:26 +0300

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -10,11 +10,14 @@ print("Welcome to GoonOS, here is a quick list of commands:")
 term.setTextColor( colors.green )
 
 print("engine - maintains a redstone signal on prefered side of the computer")
+print("")
 print("goodbye - shuts down the computer")
+print("")
+print("anything else - allows you to use the computer for other things")
 
+term.setCursorPos(getCursorPos + 2, 1)
+term.setTextColor( colors.gold )
 input = read()
-
-term.setTextColor( colors.white )
 
 if input == "engine" then
     os.run({}, "/GoonOS/engine")
@@ -22,4 +25,10 @@ end
 
 if input == "goodbye" then
     os.shutdown()
+end
+
+if input ~= "engine" or input ~= "goodbye" then
+    term.clear()
+    term.setCursorPos(1, 1)
+    term.setTextColor( colors.white )
 end
