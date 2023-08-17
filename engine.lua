5c23c40dcaeb0c142bd8cf4e29d72ce8fb42b7fc
Tucanu <84274679+Tucanu10@users.noreply.github.com>
2023-08-17 06:06:04 +0300

    Update engine.lua

---
+++ engine.lua
@@ -0,0 +1,27 @@
+-- Engine starting / stopping script
+-- Written by Tucanu
+
+local status = rs.getOutput("back") --Change to appropiate side of computer
+
+term.clear()
+
+if status == true then
+    print("Stopping the engine")
+else
+    print("Firing up the engine")
+end
+
+sleep(5)
+
+rs.setOutput("back", not status)    --Change to appropiate side of computer
+
+term.write(textutils.formatTime(os.time(ingame), true))
+term.write(" : ")
+
+if status == false then
+    term.setTextColor( colors.green)
+    print("The engine has started!")
+else
+    term.setTextColor( colors.red )
+    print("The engine has stopped")
+end
